ea0d00c3827a976b84d1843f071d7eb76c3f175d
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-25 09:54:35 +0900

    fix

--- App.jsx
+++ App.jsx
@@ -2,6 +2,12 @@ const React = require('react');
 
 require('./index.pcss');
 
+require('@babel/polyfill');
+require('core-js/stage/4');
+require('core-js/stage/3');
+require('core-js/stage/2');
+require('core-js/stage/1');
+
 module.exports = class App extends React.Component {
 	constructor(props) {
 		super(props);
